0ddfd75517e9498b3a07ebd88dca91b0730691c7
Awoo56709 <62405610+Awoo56709@users.noreply.github.com>
2025-08-26 11:50:08 -0700

    Added Files

---
+++ eds214_day1_interactive.R
@@ -0,0 +1,6 @@
+#Need to import the data
+
+read.csv(here::here("data","QuebradaCuenca1-Bisley.csv"))
+read.csv(here::here("data","QuebradaCuenca2-Bisley.csv"))
+read.csv(here::here("data","QuebradaCuenca3-Bisley.csv"))
+
